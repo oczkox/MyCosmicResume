466008d4f877e7e041d5c08dea20f4fdf72222f5
Oczkox <oczkox@gmail.com>
2018-12-03 12:19:03 +0100

    ProgressBars onScroll
    new js file

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Fad-out Slides during the scrolling
+$(function() {
+    var documentEl = $(document),
+        fadeElem = $('.fade-scroll');
+    documentEl.on('scroll', function() {
+        var currScrollPos = documentEl.scrollTop();
+
+        fadeElem.each(function() {
+            var $this = $(this),
+                elemOffsetTop = $this.offset().top;
+            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/55);
+        });
+    });
+});
+// Progress Bar Animation
+$(window).scroll(function() {
+    var scrollOffset = $(document).scrollTop();
+    var containerOffset = $('#ProgressBars').offset().top - window.innerHeight;
+    if (scrollOffset > containerOffset) {
+        $(window).off("scroll");
+        $('.progress-bar').each(function() {
+            var valueNow = $(this).attr('aria-valuenow');
+            $(this).animate({
+            width: valueNow + '%',
+            percent: 100,
+            }, {
+                duration: 2000,
+                easing: "swing",
+            progress: function(a, p, n) {
+                $(this)
+                .css('width', (valueNow * p + '%'));
+                }
+            });
+        });
+    }
+});
+
+
