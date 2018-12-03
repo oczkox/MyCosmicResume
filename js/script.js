fe16c7e6c613cc934476c21c7b7bd5bfec8c613d
Oczkox <oczkox@gmail.com>
2018-12-03 13:23:54 +0100

    changed the Fade-Scroll Value
     - fix bug

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(function() {
         fadeElem.each(function() {
             var $this = $(this),
                 elemOffsetTop = $this.offset().top;
-            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/500);
         });
     });
 });
